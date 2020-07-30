1151bf4b6e391c66a4c843a0b526ac35af2dd0ac
Amanda Brelaz <44024412+amandabrelaz@users.noreply.github.com>
2020-07-30 18:20:58 +0100

    Using Spread operator The code was changed using the spread operator, allowing to reduce it on the holdChange function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,63 +9,54 @@ function App() {
 
   const [click, setClick] = useState("");
 
+  function holdChange(event) {
+    const { name, value } = event.target;
 
-  function holdChange(event){
-
-    //hold the user's input name property and value property
-    const {name, value} = event.target;
-
-    setContact((prevValue) => {
-
-      if( name === "userfName"){
-        return{
-          //only modifies fName object
-          fName : value,
-          lName : prevValue.lName,
-          email : prevValue.email
-        }
-      }
-      else if ( name === "userlName"){
-        return{
-          fName : prevValue.fName,
-          lName : value,
-          email : prevValue.email
-        }
-      }
-      else if ( name === "useremail"){
-        return{
-          fName : prevValue.fName,
-          lName : prevValue.lName,
-          email : value
-        }
-      }
+    //Spread Operator
+    setContact(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
     });
   }
-  
 
-
-  function handleClick(event){
-
-    setClick([(contact.fName) , (contact.lName), (contact.email)]);    
+  function handleClick(event) {
+    setClick([contact.fName, contact.lName, contact.email]);
     event.preventDefault();
   }
 
-
-
-
   return (
     <div className="container">
-      <h1> Hello {click[0]} {click[1]}</h1>
+      <h1>
+        {" "}
+        Hello {click[0]} {click[1]}
+      </h1>
       <p>email: {click[2]}</p>
 
-      <form onSubmit = {handleClick}>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={holdChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={holdChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={holdChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
 
-
-        <input onChange= {holdChange} name="userfName" value={contact.fName} placeholder="First Name" />
-        <input onChange= {holdChange} name="userlName" value={contact.lName} placeholder="Last Name" />
-        <input onChange= {holdChange} name="useremail" value={contact.email} placeholder="Email" />
-   
-        <button onClick= {handleClick} type="submit">Submit</button>
+        <button onClick={handleClick} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
